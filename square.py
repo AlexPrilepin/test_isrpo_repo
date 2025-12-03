8b5ff238633442d0b30c9f131a9c56b996d29717
AlexPrilepin <111271681+AlexPrilepin@users.noreply.github.com>
2025-12-03 13:56:45 +0300

    Add files via upload

---
+++ square.py
@@ -0,0 +1,35 @@
+
+def area(a):
+    """
+    Вычисляет площадь квадрата по заданной длине стороны.
+        Параметры:
+            a (float): Длина стороны квадрата
+        Возвращает:
+            float: Площадь квадрата.
+    """
+
+    if not isinstance(a, (int, float)):
+      raise TypeError("Ожидается int или float")
+    
+    if a < 0:
+      raise ValueError("Число не может быть отрицательным")
+
+    return a * a * 52
+
+
+def perimeter(a):
+    """
+    Вычисляет периметр квадрата по заданной длине стороны.
+        Параметры:
+            a (float): Длина стороны квадрата.
+        Возвращает:
+            float: Периметр квадрата.
+    """
+
+    if not isinstance(a, (int, float)):
+      raise TypeError("Ожидается int или float")
+    
+    if a < 0:
+      raise ValueError("Число не может быть отрицательным")
+    
+    return 4 * a
